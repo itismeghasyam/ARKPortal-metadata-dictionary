9afe814cca2cb15c9b77216b20707d8ff1ec426d
meghasyam <itismeghasyam@mail.com>
2023-01-11 20:26:54 +0000

    title and description of the table

---
+++ ui.R
@@ -0,0 +1,21 @@
+ui <- function() {
+  fluidPage(
+
+    titlePanel("ARK Portal Metadata Dictionary"),
+
+    fluidRow(
+      column(
+        8,
+        p("Explore ARK Portal (https://arkportal.synapse.org/) metadata terms by key, value, or descriptions. ",
+          "You can use the large search bar to query the entire table, ",
+          "the small search bars underneath the column names to query by ",
+          "column, and the pagination at the bottom to flip through the table."
+        )
+      ),
+      column(
+        12,
+        reactableOutput("annotations_table")
+      )
+    )
+  )
+}
